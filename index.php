306a8523fa0b831680aa6ea7d6102f83167d4c9b
Sergey <dungeonmir@gmail.com>
2023-07-29 20:22:25 +0300

    small css changes

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require_once('autoload.php');
 
 echo '
 <style>
+	h1,h3,p,a{
+		font-family:  sans-serif; 
+	}	
 	.post{
 	background-color: lightgray;
 	padding: 15px;
@@ -15,7 +18,13 @@ echo '
 	gap: 10px;
 	}
 	h1{
-		color: #111; font-family: "Helvetica Neue", sans-serif; font-size: 60px; font-weight: bold; letter-spacing: -1px; line-height: 1; text-align: center; 
+		color: #111; 
+		font-family: "Helvetica Neue", sans-serif; 
+		font-size: 60px; 
+		font-weight: bold; 
+		text-align: center; 
+		margin-top: 20px;
+		margin-bottom: 0;
 	}
 	.invisible{
 	opacity: 0;
@@ -23,23 +32,51 @@ echo '
 	.invisible:hover{
 	    opacity: 1;
 	}
+	a {
+		background-color: darkgray;
+		color: white;
+		padding: 5px 10px;
+		text-decoration: none;
+		text-transform: uppercase;
+	 }
+	 
+	a:hover {
+		background-color: #555;
+	 }
+	 
+	a:active {
+		background-color: black;
+	 }
+	 
+	a:visited {
+		background-color: #ccc;
+	 }
+	 .links{
+	display: flex;
+	gap: 10px;
+	flex-wrap: wrap; 
+	margin: 20px;
+	}
 </style>
 <H1>Посты</H1>';
 
+echo '<div class="links">';
 echo '<a href="./addPost.php">Добавить пост</a>';
 echo '<a href="./changePost.php">Изменить пост</a>';
 echo '<a href="./deletePost.php">Удалить пост</a>';
 echo '<a href="./posts.php">Все посты</a>';
 echo '<a href="./post.php">Определенный пост</a>';
+echo '</div>';
 
-echo '</br>';
+echo '<div class="links">';
 echo '<a href="./todo.php">Определенное задание</a>';
 echo '<a href="./todos.php">Все задания</a>';
+echo '</div>';
 
-echo '</br>';
+echo '<div class="links">';
 echo '<a href="./user.php">Определенный пользователь</a>';
 echo '<a href="./users.php">Все пользователи</a>';
-
+echo '</div>';
 $post = new Post();
 $json = $post->getPosts();
 $posts = json_decode($json);
