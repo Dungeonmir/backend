16c8d93e979a93bc04ca9b24b44345d8f0254efd
Sergey <dungeonmir@gmail.com>
2023-07-29 22:17:02 +0300

    change readme

--- addPost.php
+++ addPost.php
@@ -2,6 +2,6 @@
 require_once('./autoload.php');
 header("Content-type: application/json; charset=utf-8");
 $post = new Post;
-$data = $post->addPost(4, 'Title of the post', 'This post is about something, this is post`s body');
+$data = $post->addPost(4, 'Title of the post', 'Post body');
 
 echo $data;
